ad5db111751e35d46cbd07c04746b1a43e0dd54a
cabralotavio3 <bugvug250@gmail.com>
2025-05-19 12:10:41 -0300

    ...

---
+++ init.php
@@ -0,0 +1,12 @@
+<?php
+// constantes com as credenciais de acesso ao banco MySQL
+define('DB_HOST', 'localhost');
+define('DB_USER', 'USUARIO_COM_ACESSO_AO_BD');
+define('DB_PASS', 'SENHA_DO_USUARIO');
+define('DB_NAME', 'NOME_DA_BASE_DE_DADOS');
+
+date_default_timezone_set('America/Sao_Paulo');
+
+// inclui o arquivo de funções
+require_once 'funcoes.php';
+?>
